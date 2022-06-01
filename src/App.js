d4f87efcb03db34be90aab13c90b7bb6bd127131
Mohit Sojitra <mohitsojitra0@gmail.com>
2022-06-01 17:38:06 +0530

    material icon added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import AbcIcon from "@mui/icons-material/Abc";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <AbcIcon />
+        <AccountBalanceIcon />
         <a
           className="App-link"
           href="https://reactjs.org"
